7a21a29643aee1b8d449f9734b6a426b920e3c75
Hà Hải Quân <quanhh@hiworld.com.vn>
2016-06-14 15:40:25 +0700

    Upload project

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,28 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def collections
+
+  end
+
+  def new_arrivals
+
+  end
+
+  def sales
+
+  end
+
+  def accessories
+
+  end
+
+  def about_us
+
+  end
+
+  def contact
+
+  end
+end
