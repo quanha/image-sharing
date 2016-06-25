e52e5ef4b442d9fba92a51077b08d5481d8e95be
quanha <quanhh.dev@gmail.com>
2016-06-25 10:56:42 +0700

    Added has many form product quantity

--- app/assets/javascripts/admin/remove_field.js
+++ app/assets/javascripts/admin/remove_field.js
@@ -1,4 +1,17 @@
-$('.remove_product_quantity').on('click', function(){
-    $(this).prev('input[type=hidden]').val('1');
-    $(this).closest('tr').hide()
+function remove_product(){
+    $('.remove_product_quantity').bind('click', function(){
+        $(this).prev('input[type=hidden]').val('1');
+        $(this).closest('tr').hide()
+    })
+}
+
+$('.add_product_quantity').on('click', function(){
+    var time = new Date().getTime();
+    var regexp = new RegExp($(this).data('id'), 'g');
+    $('.product_quantity_table').append($(this).data('fields').replace(regexp, time));
+    remove_product();
+})
+
+$(document).ready(function(){
+    remove_product()
 })
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def title(text)
     content_for :title, text
   end
+
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.send(association).klass.new
+    id = new_object.object_id
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render('shared/admin/product_quantity', f: builder)
+    end
+    link_to(name, '#/', class: 'btn btn-sm btn-success add_product_quantity', data: {id: id, fields: fields.gsub('\n', '')})
+  end
 end
