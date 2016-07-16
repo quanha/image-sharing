6111efdc3fa32a6e584e51ccceaef7238bfdbf7a
quanhh <quanhh@hiworld.com.vn>
2016-07-16 10:20:09 +0700

    Changed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-  User.create(email: 'admin@gmail.com', password: 'mothaiba', password_confirmation: 'mothaiba', first_name: 'Smart', last_name: 'Admin', role: 'admin')
+  User.create(email: 'admin@example.com', password: '12345678', password_confirmation: '12345678', first_name: 'Smart',
+              last_name: 'Admin', role: 'admin', confirmation_token: nil, confirmed_at: DateTime.strptime("09/14/2009 8:00", "%m/%d/%Y %H:%M"))
   Category.create(name: 'Collections', description: 'Collection of items', at_homepage: 1)
