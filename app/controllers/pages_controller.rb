31ea333792ae29a3d79a3e6c86a7944c7684db54
quanhh <quanhh@hiworld.com.vn>
2016-08-11 16:52:55 +0700

    Fix bugs at collections

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,7 +14,7 @@ class PagesController < ActionController::Base
     product_type_query = (params[:product_type_id].to_i != 0) ? "products.product_type_id = #{params[:product_type_id]}" : nil
     @category = Category.find(params[:category_id])
     @products = Product.joins(:product_categories).where(product_categories: {category_id: params[:category_id]}).where(product_type_query)
-    @product_types = ProductType.joins(products: :product_categories).where(product_categories: {category_id: params[:category_id]}).group(:name)
+    @product_types = ProductType.joins(products: :product_categories).where(product_categories: {category_id: params[:category_id]}).group([:id, :name])
   end
 
   def new_arrivals
