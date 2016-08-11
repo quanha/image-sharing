56e6de1795d243bb201a34f800c0d892e259b34f
quanhh <quanhh@hiworld.com.vn>
2016-08-11 16:47:34 +0700

    Fix bugs at collections

--- vendor/assets/javascripts/user_template.js
+++ vendor/assets/javascripts/user_template.js
@@ -1,6 +1,5 @@
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require ./user/easyResponsiveTabs.js
 //= require ./user/owl.carousel.js
 //= require ./user/responsiveslides.min.js
