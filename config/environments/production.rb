c711623c3d5e5c9b26980947616fe0443419ee52
quanhh <quanhh@hiworld.com.vn>
2016-08-14 22:15:51 +0700

    Fixed email sending 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -86,7 +86,7 @@ Rails.application.configure do
       :path => '/:class/:attachment/:id_partition/:style/:filename'
   }
 
-  config.action_mailer.default_url_options = { :host => ENV.fetch('HOST') }
+  config.action_mailer.default_url_options = { :host => 'gaiashop.heroku.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
